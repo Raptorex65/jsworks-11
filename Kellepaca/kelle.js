2c56d06c1a9b5c2ea7fe58ace6e80b9d825ad7b2
Raptorex65 <71727239+Raptorex65@users.noreply.github.com>
2020-12-16 23:33:12 +0100

    "Düzeltme"

--- Kellepaca/kelle.js
+++ Kellepaca/kelle.js
@@ -1,6 +1,20 @@
-let ciftlik = ["inek","tavuk","koyun","tavuk","domuz","inek","koyun", "koyun"];
-let hayvanBacakSayisi = 0;
+/* Ciftcimiz anlasmali oldugu kelle pacaci Necip Ustaya gercek zamanli istatistik vermek istiyor. Gayet müslüman olan Necip Usta ise, sordugu anda kac tane hayvan bacagi oldugu bilgisini almak istiyor.
 
+Program: 
+Ciftlikteki hayvanlar bir array icerisinde karisik sekilde tutulacak ve bu ciftlikte sadece su hayvanlar var:
+- inek
+- tavuk
+- domuz
+- koyun
+
+Array örnegi: [“inek”, “inek”, “tavuk”, “domuz”, “inek”]
+Yapacaginiz program verilen array üzerinden kullanilabilir hayvan bacagi sayisini bulacak ve ekrana bastiracak.
+*/
+
+
+let ciftlik = ["inek","tavuk","koyun","tavuk","domuz","inek","koyun", "koyun"];//ciftlikte varolan hayvanlar
+let hayvanBacakSayisi = 0;
+// Sadece Koyun ve İneklerin bacak sayıları toplamını bulacak döngümüz:
 for(let index = 0; index < ciftlik.length; index++){
     if(ciftlik[index] == "koyun"){
         hayvanBacakSayisi = hayvanBacakSayisi + 4;
